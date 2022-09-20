bb4a4ed20424267dab58187c8efa3a3365b6b055
Nicolas <79662506+Klipfel-Nicolas@users.noreply.github.com>
2022-09-20 21:10:38 +0200

    Config, Structure and Style

---
+++ app.js
@@ -0,0 +1,143 @@
+require('dotenv').config()
+
+const logger = require('morgan')
+const express = require('express')
+const errorHandlers = require('errorhandler')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const app = express()
+const path = require('path')
+const port = 3000
+
+app.use(logger('dev'))
+app.use(errorHandlers())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride())
+app.use(express.static(path.join(__dirname, 'public')))// static files deliver (scss...)
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  })
+}
+
+const handleLinkResolver = doc => {
+  if (doc.type === 'product') {
+    return `/detail/${doc.uid}`
+  }
+
+  if (doc.type === 'collections') {
+    return '/collections'
+  }
+
+  if (doc.type === 'about') {
+    return '/about'
+  }
+
+  return '/'
+}
+
+app.use((req, res, next) => {
+  // Use for CMS links (like in collections.pug)
+  res.locals.Link = handleLinkResolver
+
+  // Method used in collection.pug
+  res.locals.Numbers = index => {
+    return index === 0 ? 'One' : index === 1 ? 'Two' : index === 2 ? 'Three' : index === 3 ? 'Four' : ''
+  }
+
+  res.locals.PrismicDOM = PrismicDOM
+
+  next()
+})
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+const handleRequest = async api => {
+  const metadata = await api.getSingle('metadata')
+  const navigation = await api.getSingle('navigation')
+  const preloader = await api.getSingle('preloader')
+
+  return {
+    metadata,
+    navigation,
+    preloader
+  }
+}
+
+app.get('/', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  const home = await api.getSingle('home')
+
+  const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
+    fetchLinks: 'product.image'
+  })
+
+  res.render('pages/home', {
+    ...defaults,
+    collections,
+    home
+  })
+})
+
+/**
+ * About
+ */
+app.get('/about', async (req, res) => {
+  const api = await initApi(req)
+  const about = await api.getSingle('about')
+  const defaults = await handleRequest(api)
+
+  res.render('pages/about', {
+    ...defaults,
+    about
+  })
+})
+
+/**
+ * Detail
+ */
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  console.log(defaults)
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title'
+  })
+
+  res.render('pages/detail', {
+    ...defaults,
+    product
+  })
+})
+
+/**
+ * Collections
+ */
+app.get('/collections', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+  const home = await api.getSingle('home')
+
+  const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
+    fetchLinks: 'product.image'
+  })
+
+  res.render('pages/collections', {
+    ...defaults,
+    home,
+    collections
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
